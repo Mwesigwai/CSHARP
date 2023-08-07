9e919d53665f5fd29b446daa037d1591a1445865
Mwesigwa isaac <mwesigwai433@gmail.com>
2023-08-07 13:26:03 +0300

    modified the file and added a new program to find random numbers like for a dice game

--- calc/Program.cs
+++ calc/Program.cs
@@ -1,2 +1,4 @@
 ﻿using System;
-Console.WriteLine("hello world\n I am learning c sharp with git");
+using projLib;
+Console.WriteLine("Rolling dice game");
+Class1.RollDice();
--- projLib/Class1.cs
+++ projLib/Class1.cs
@@ -1,5 +1,10 @@
 ﻿namespace projLib;
 public class Class1
 {
-
+    public static void RollDice()
+    {
+        Random randomDiceNum = new Random();
+        int randomNum = randomDiceNum.Next(1,6);
+        Console.WriteLine("You rolled number: " + randomNum);
+    }
 }
